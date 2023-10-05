498781d8a163222e65d7098ab7c2b2bbac604fe3
Esteban Sepulveda <tebansd@icloud.com>
2023-10-04 21:29:23 -0700

    initial project setup

--- src/store.js
+++ src/store.js
@@ -2,5 +2,5 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
 
 export const store = configureStore({
-    reducer: counterReducer
+    reducer: counterReducer,
 })
